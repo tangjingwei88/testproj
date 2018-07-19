4c043bf5b189d928f1341e26c13956181505efd3
tangjingwei88 <jingweitang88@gmail.com>
2018-07-19 19:11:38 +0800

    UI框架

---
+++ UIFrameWork/Assets/Resource/Scripts/UIFrame/UIBasePanel.cs
@@ -0,0 +1,24 @@
+﻿/***
+ * Author:TSoft
+ * Title:  Panel基类：UIBasePanel  
+ * Date: 18-7-19
+ * 
+ * **/
+
+using UnityEngine;
+using System.Collections;
+
+
+public class UIBasePanel {
+
+    public UIPanelType PanelType {
+        get;
+        private set;    
+    }
+
+    public UIBasePanel(UIPanelType panelType)
+    {
+        PanelType = panelType;
+    }
+
+}
